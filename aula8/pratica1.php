987bf2b93ab5ca30ba8f7dc6f24508362a6abc94
PingoStar <166458195+PingoStar@users.noreply.github.com>
2025-09-26 21:56:09 -0300

    Aula 8

---
+++ aula8/pratica1.php
@@ -0,0 +1,6 @@
+<?php 
+    define("nome","Anderson");
+    define("sobrenome","Moreira");
+    $nome = "Anderson Moreira";
+    echo $nome;
+?>
---
+++ aula8/pratica5.php
@@ -0,0 +1,16 @@
+<?php
+
+$disciplina = array("Engenharia de Software" , "Algoritmo 2" , "Gerência de Projetos" , "Banco de Dados" , "Programação Web");
+$professor = array("Jullian" , "Fernando" , "Jullian" , "Marcos" , "Cleber");
+    
+      for ($i = 0; $i < count($disciplina); ++$i)  {
+            echo "Disciplina: " . $disciplina[$i] . ", Professor: " . $professor[$i]  ;
+      }
+
+
+
+
+
+
+
+
